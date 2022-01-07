87b605d1b2dc6a27ccbcdd9f23e02691844ddfe6
Brian Kim <bkim8815@gmail.com>
2022-01-06 19:58:22 -0500

    initial commit

---
+++ ingestion.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import os
+import json
+from datetime import datetime
+
+
+
+
+#############Load config.json and get input and output paths
+with open('config.json','r') as f:
+    config = json.load(f)
+
+input_folder_path = config['input_folder_path']
+output_folder_path = config['output_folder_path']
+
+
+
+#############Function for data ingestion
+def merge_multiple_dataframe():
+    #check for datasets, compile them together, and write to an output file
+    directories=[input_folder_path]
+    df_list = pd.DataFrame(columns=['corporation','lastmonth_activity','lastyear_activity','number_of_employees','exited'])
+
+    for directory in directories:
+        filenames = os.listdir(os.getcwd()+"/"+directory)
+        print(filenames)
+        f = open("ingestedfiles.txt", "a")
+        f.write(str(filenames))
+        f.close()
+
+        for each_filename in filenames:
+            if each_filename.endswith('.csv'):
+                df1 = pd.read_csv(os.getcwd()+"/"+directory+"/"+each_filename)
+                df_list=df_list.append(df1)
+
+
+    result=df_list.drop_duplicates()
+    result.to_csv(output_folder_path + '/finaldata.csv', index=False)
+
+
+
+if __name__ == '__main__':
+    merge_multiple_dataframe()
